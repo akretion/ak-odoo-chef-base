fb469d294d5e518572d49acd91c7fa9ddabfd894
Raphaël Valyi <rvalyi@gmail.com>
2013-02-03 02:44:59 -0200

    doc/metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,15 @@
-maintainer       "Akretion"
-maintainer_email "contact@akretion.com"
-license          "All rights reserved"
-description      "Prepares the server before installing OpenERP"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "1.0.0"
-depends          "python"
-depends          "apt"
-depends          "ak-tools"
+name              "Akretion OpenERP Base"
+maintainer        "Akretion"
+maintainer_email  "contact@akretion.com"
+license           "Apache 2.0"
+description       "Provisions server environment for OpenERP"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0.0"
+recipe            "ak-openerp-base::default", "Default"
+depends           "python"
+depends           "apt"
+depends           "ak-tools"
+
+%w{ ubuntu }.each do |os|
+  supports os
+end

92576efd09dd982d31146ab532b918e7c70200b8
Raphaël Valyi <rvalyi@gmail.com>
2013-02-24 23:20:07 -0300

    typo

--- recipes/dev_servers.rb
+++ recipes/dev_servers.rb
@@ -1,7 +1,7 @@
 (node[:openerp][:dev_servers] || []).each do |name, remote|
   execute "rsync-#{name}" do
     command "rsync -az #{remote} #{name}"
-    creates "/home/#{node[:openerp][:dev][:unix_user]}/name"
+    creates "/home/#{node[:openerp][:dev][:unix_user]}/#{name}"
     cwd "/home/#{node[:openerp][:dev][:unix_user]}"
     user node[:openerp][:dev][:unix_user]
   end

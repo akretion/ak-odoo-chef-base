6b10aea7824c2fa8a8da0fdd49d1d55538d02f42
Raphaël Valyi <rvalyi@gmail.com>
2013-03-02 02:26:10 -0300

    attempt to fix dev hosts

--- recipes/dev_servers.rb
+++ recipes/dev_servers.rb
@@ -5,4 +5,20 @@
     cwd "/home/#{node[:openerp][:dev][:unix_user]}"
     user node[:openerp][:dev][:unix_user]
   end
+
+  host = remote.split("@")[1].split(":")[0]
+  ip = `nslookup #{host}`.split('Address: ').last.gsub(/\n/, '')  
+
+  execute "parent_host-#{name}" do
+    command "echo '#{ip}  #{name}_parent_host' >> /etc/hosts"
+    user "root"
+    not_if "grep '#{name}_parent_host' /etc/hosts", :user => "root", :group => "root"
+  end
+
+  execute "stacking_host-#{name}" do
+    command "echo '#{ip}  #{name}_stacking_host' >> /etc/hosts"
+    user "root"
+    not_if "grep '#{name}_stacking_host' /etc/hosts", :user => "root", :group => "root"
+  end
+
 end

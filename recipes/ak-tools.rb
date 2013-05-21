80298379c0dc49d9c7325b1f29a9d7deee7725e9
Raphaël Valyi <rvalyi@gmail.com>
2013-05-21 11:48:57 -0300

    disabled patch-S3-progressbar convenience patch for now as it's not robust against ubuntu and s3md versions

--- recipes/ak-tools.rb
+++ recipes/ak-tools.rb
@@ -66,7 +66,7 @@ package "s3cmd" do
   notifies :run, "execute[patch-S3-progressbar]", :immediately #TODO adapt ak-db command just for simple S3 retrieval
 end
 
-execute "patch-S3-progressbar" do #hacky workaround I found so that using it in Ruby displays the transfer progresses
-  command """sed -i 's/def update(self, current_position = -1, delta_position = -1):/def update(self, current_position = -1, delta_position = -1):\\n                sys.stdout.write(chr(10))/' /usr/share/s3cmd/S3/Progress.py"""
-  action :nothing
-end
+#execute "patch-S3-progressbar" do #hacky workaround I found so that using it in Ruby displays the transfer progresses
+#  command """sed -i 's/def update(self, current_position = -1, delta_position = -1):/def update(self, current_position = -1, delta_position = -1):\\n                sys.stdout.write(chr(10))/' /usr/share/s3cmd/S3/Progress.py"""
+#  action :nothing
+#end

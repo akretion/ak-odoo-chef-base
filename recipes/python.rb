7d92971acea69eec9e0a013ad7450abf398b62c1
Raphaël Valyi <rvalyi@gmail.com>
2014-05-12 21:13:22 -0300

    fix bug and allows to pin global python pip packages

--- recipes/python.rb
+++ recipes/python.rb
@@ -1,5 +1,12 @@
-include_recipe "python::pip" #TODO add missing for v7
-#TODO deal with versions if item is a hash
-(node[:openerp][:pip_packages] + node[:openerp][:pip_packages_extra] node[:openerp][:pip_packages_env]).each do |pack|
-  python_pip(pack) { action :install }
+include_recipe "python::pip"
+
+(node[:openerp][:pip_packages] + node[:openerp][:pip_packages_extra] + node[:openerp][:pip_packages_env]).each do |pack|
+  if pack.index('=') # TODO see if we can support more constraints if python_pip supports it
+    items = pack.gsub('==', '=').split('=')
+    python_pip items[0] do
+      version items[1]
+    end
+  else
+    python_pip(pack)
+  end
 end

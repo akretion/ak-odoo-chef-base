c18d643b8b240a88d4844156331b57c637f25231
Raphaël Valyi <rvalyi@gmail.com>
2014-06-28 10:14:58 -0300

    fixes bad wkhtmltopdf update

--- recipes/wkhtmltopdf.rb
+++ recipes/wkhtmltopdf.rb
@@ -11,6 +11,6 @@ else
 end
 
 execute 'install wkhtmltopdf' do
-  command "wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.1/wkhtmltox-0.12.1_linux-#{release}-#{arch}.deb; dpkg -i wkhtmltox-0.12.1-9615f00_linux-#{release}-#{arch}.deb"
+  command "wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.1/wkhtmltox-0.12.1_linux-#{release}-#{arch}.deb; dpkg -i wkhtmltox-0.12.1_linux-#{release}-#{arch}.deb"
   not_if {`which wkhtmltopdf` != ''}
 end

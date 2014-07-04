a20b3cb30440a2711aab0ab4f770fb963af98934
Raphaël Valyi <rvalyi@gmail.com>
2014-07-04 01:57:40 -0300

    no spurious Postgres user change when it's already correct

--- definitions/postgresql_user.rb
+++ definitions/postgresql_user.rb
@@ -4,6 +4,9 @@ define :postgresql_user, :action => :create do
   when :create
     privileges = { :superuser => false, :createdb => false, :inherit => true, :login => true }
     privileges.merge! params[:privileges] if params[:privileges]
+    bool_map = {true => "t", false => "f"}
+    exists = "/usr/bin/psql -c 'SELECT usename FROM pg_catalog.pg_user' | grep '^ #{params[:name]}$'"
+    is_equal = "/usr/bin/psql -c 'SELECT usesuper,usecreatedb,usename FROM pg_catalog.pg_user' | grep '^ #{bool_map[privileges[:superuser]]}        | #{bool_map[privileges[:createdb]]}           | #{params[:name]}$'"
     privileges = privileges.to_a.map! { |p,b| (b ? '' : 'NO') + p.to_s.upcase }.join(' ')
 
     if params[:password]
@@ -11,18 +14,18 @@ define :postgresql_user, :action => :create do
     end
 
     sql = "#{params[:name]} #{privileges} #{password}"
-    exists = "/usr/bin/psql -c 'SELECT usename FROM pg_catalog.pg_user' | grep '^ #{params[:name]}$'"
 
     execute "alter postgresql user #{params[:name]}" do
       user "postgres"
       command "/usr/bin/psql -c \"ALTER ROLE #{sql}\""
-      only_if exists, :user => "postgres"
+      only_if exists, user: "postgres"
+      not_if is_equal, user: "postgres"
     end
 
     execute "create postgresql user #{params[:name]}" do
       user "postgres"
       command "/usr/bin/psql -c \"CREATE ROLE #{sql}\""
-      not_if exists, :user => "postgres"
+      not_if exists, user: "postgres"
     end
   when :drop
     execute "/usr/bin/psql -c 'DROP ROLE IF EXISTS #{params[:name]}'" do
